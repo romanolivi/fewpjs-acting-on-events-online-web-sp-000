f7363a1ecbba3cd42e66d16dc6f5f9d4efd493b5
romanolivi <romano_livi@outlook.com>
2021-03-16 14:16:20 -0400

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
 
-    if (left > 0) {
+    if (left < 360) {
         dodger.style.left = `${left + 1}px`;
     }
 }
